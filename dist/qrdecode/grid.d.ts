0e1f6238587a870608bf39ac1ba44bf89d878f81
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2018-02-12 15:58:27 +0100

    Uploading dist folder for package.json reference until official update is release on npmjs

---
+++ dist/qrdecode/grid.d.ts
@@ -0,0 +1,7 @@
+export declare class GridSampler {
+    width: number;
+    height: number;
+    constructor(width: any, height: any);
+    checkAndNudgePoints(image: any, points: any): void;
+    sampleGrid3(image: any, rawImage: any, dimension: any, transform: any): any;
+}
