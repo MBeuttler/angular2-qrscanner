54027d31966cd643c5e100366c5eeacf15e3a6dc
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2018-02-12 16:03:49 +0100

    Moving dist folder to root.

--- dist/qrdecode/grid.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class GridSampler {
-    width: number;
-    height: number;
-    constructor(width: any, height: any);
-    checkAndNudgePoints(image: any, points: any): void;
-    sampleGrid3(image: any, rawImage: any, dimension: any, transform: any): any;
-}
